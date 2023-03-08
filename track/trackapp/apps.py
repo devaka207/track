3d5615f9b32a751ca51bb8cf53e51e94e3dac93d
Akshay Hegde <34371045+devaka207@users.noreply.github.com>
2023-03-08 23:18:33 +0530

    Delete track directory

--- track/trackapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrackappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trackapp'
--- track/trackapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Loc(models.Model):
-    latitude = models.CharField(max_length=120)
-    longitude = models.CharField(max_length=120)
-
-    def __str__(self):
-        return self.latitude,self.longitude
